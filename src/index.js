e02bc05f9c967886f9be910eabdb04dadfd8e975
Stiflerzak <hilarymwendia97@gmail.com>
2023-01-05 19:17:48 +0300

    Refactored book component

--- src/index.js
+++ src/index.js
@@ -14,24 +14,12 @@ const Booklist=() =>{
 const Book=() =>{
     return (
         <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src="https://images-na.ssl-images-amazon.com/images/I/91MowzF+WZL._AC_UL600_SR600,400_.jpg" alt="The Light We Carry: Overcoming in Uncertain Times"/>
+        <h2>The Light We Carry: Overcoming in Uncertain Times</h2>
+        <h4>Michelle Obama</h4>
         </article>
         )
 }
-const Image=() => <img src="https://images-na.ssl-images-amazon.com/images/I/91MowzF+WZL._AC_UL600_SR600,400_.jpg" alt="The Light We Carry: Overcoming in Uncertain Times"/>
-const Title=() => <h2>The Light We Carry: Overcoming in Uncertain Times</h2>
-// Author component 
-const Author=() => {
-    // inlinestyling using an object
-    const inlineHeadingStyles={
-    color:'#617d98',
-    fontSize:'0.75rem',
-    marginTop:"0.5rem" }
-   return <h4 style={inlineHeadingStyles}>Michelle Obama</h4>
-}
-
 
 const root =ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Booklist/>)
