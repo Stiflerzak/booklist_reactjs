5edf07c584558bb45c64a1cbdd844b317e486730
Stiflerzak <hilarymwendia97@gmail.com>
2023-01-05 17:41:45 +0300

    Added booklist component

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const Booklist=() =>{
+    return (
+        <section>
+        <Book/>
+        <Book/>
+        <Book/>
+        </section>
+    )
+}
+const Book=() =>{
+    return (
+        <article>
+        <Image/>
+        <Title/>
+        <Author/>
+        </article>
+        )
+}
+const Image=() => <h2>image placeholder</h2>
+const Title=() => <h2>Book Title</h2>
+const Author=() => {
+   return <h4>Book Author</h4>
+}
+
+
+const root =ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Booklist/>)
